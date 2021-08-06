6e8d3d630327cdd805caa52b9db7aef471a08eb2
DylanLeeee <412191806@qq.com>
2021-08-06 23:55:35 +0800

    browser

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
   <div id="app">
     <h1>jd</h1>
     <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button plain type="primary">朴素按钮</van-button>
+    <van-button plain type="info">朴素按钮</van-button>
+    <van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+  </van-cell-group>
   </div>
 </template>
-<script>
 
+<script>
 export default {
   name: 'App'
 }
 </script>
 
-<style lang="less">
-</style>
+<style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import store from './store'
 //   加载全局样式
 import './styles/index.less'
 
+//  加载vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 设置REM
+import 'amfe-flexible'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
