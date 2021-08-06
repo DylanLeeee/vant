e37fac6f711c76ed6a3d949b337ec221ea96b227
DylanLeeee <412191806@qq.com>
2021-08-07 01:36:47 +0800

    finish framework

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,42 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/login/')
+    },
+    {
+        path: '/',
+        component: () => import('@/views/layout/'),
+        children: [
+            {
+                path: '',
+                name: 'home',
+                component: () => import('@/views/home/')
+            },
+            {
+                path: '/search',
+                name: 'search',
+                component: () => import('@/views/search/')
+            },
+            {
+                path: '/my',
+                name: 'my',
+                component: () => import('@/views/my/')
+            },
+            {
+                path: '/cart',
+                name: 'cart',
+                component: () => import('@/views/cart/')
+            }
+
+        ]
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="layout-container">
+        <!-- 子路由 -->
+        <router-view />
+        <!-- 底部导航栏-->
+        <van-tabbar v-model="active" route>
+            <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+            <van-tabbar-item icon="search" to="/search">搜索</van-tabbar-item>
+            <van-tabbar-item icon="cart-o" to="/cart">购物车</van-tabbar-item>
+            <van-tabbar-item icon="envelop-o" to="/my">我的</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            active: 1
+        }
+    }
+}
+</script>
