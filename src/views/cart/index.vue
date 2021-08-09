befb52e2540840406a4e89cb80bdd22cc1da01ef
DylanLeeee <412191806@qq.com>
2021-08-10 00:55:51 +0800

    feat: cart start

--- src/views/cart/index.vue
+++ src/views/cart/index.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>购物车</div>
+    <div class="cart-container">
+        <van-nav-bar
+            class="app-nav-bar"
+            title="购物车"
+            left-arrow
+            @click-left="$router.back()"
+        />
+        <van-submit-bar :price="3050" button-text="提交订单" @submit="onSubmit" />
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'CartIndex'
+    name: 'Cart'
 }
 </script>
